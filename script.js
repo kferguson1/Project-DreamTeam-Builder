602d517064a44e5a2937d1337a6a18c633a9db2d
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-01-06 21:30:44 -0500

    added variables for all info from JSON and for loop

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ var search = "charizard";
         url: queryURL,
         method: "GET",
       }).then(function (response) {
-        console.log(JSON.stringify(response))
-        
+        // console.log(JSON.stringify(response))
+        var pokemon = response.name;
+        console.log(pokemon)
+        var sprite = response.sprites.front_default;
+        console.log(sprite)
+        var image = response.sprites.other["official-artwork"].front_default;
+        console.log(image)
+        var number = response.id;
+        console.log(number)
+        var type1 = response.types[0].type.name;
+        console.log(type1)
+        var type2 = response.types[1].type.name;
+        console.log(type2)
+        var baseStat = response.stats;
+        // console.log(baseStat)
+        // var statName = response.stats[0].stat.name;
+        // console.log(statName)
+        for (i = 0; i < baseStat.length; i++) {
+         console.log(baseStat[i].base_stat)
+        }
       });
       
