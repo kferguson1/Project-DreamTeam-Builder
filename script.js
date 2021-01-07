280031b96ee7807b51816bc3403b8e467435c133
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-01-06 19:03:41 -0500

    added script page

---
+++ script.js
@@ -0,0 +1,10 @@
+var search = "charizard";
+      var queryURL = " https://pokeapi.co/api/v2/pokemon/" + movie;
+
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        
+        
+      });
