543f5927a0be126b12c3afc8323bf85457e2e345
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-01-06 20:22:52 -0500

    got link working for pokeapi and linked script in the html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+
 var search = "charizard";
-      var queryURL = " https://pokeapi.co/api/v2/pokemon/" + search;
+      var queryURL = " https://pokeapi.co/api/v2/pokemon/" + search + "";
 
       $.ajax({
         url: queryURL,
@@ -9,3 +9,4 @@ var search = "charizard";
         console.log(JSON.stringify(response))
         
       });
+      
