dd704f9d4cbe0e3d9a7f916e6b8768dad4af8dcc
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-01-09 13:12:31 -0500

    made changes

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-
+// the search query that will get plugged in for the response
 var search = "420";
+//api link 
       var queryURL = " https://pokeapi.co/api/v2/pokemon/" + search + "";
-
+//ajax method to get the JSON back from the api
       $.ajax({
         url: queryURL,
         method: "GET",
       }).then(function (response) {
-        // console.log(JSON.stringify(response))
+        //getting all info from JSON response
         var pokemon = response.name;
         var cap = pokemon.charAt(0).toUpperCase() + pokemon.slice(1)
         var sprite = response.sprites.front_default;
-        
         var image = response.sprites.other["official-artwork"].front_default;
-        
         var number = response.id;
-        
-        // var type1 = response.types[0].type.name;
-        
-        // var type2 = response.types[1].type.name;
-        
         var baseStat = response.stats;
-        // console.log(baseStat)
-        // var statName = response.stats[0].stat.name;
-        // console.log(statName)
+        //creating elements to put info into html
         var pokeDiv = $("<div>");
         var statDiv = $("<div>");
         var pokeImg = $("<img>");
@@ -30,9 +22,10 @@ var search = "420";
         var addButton = $("<button>")
         addButton.addClass("add-team")
         addButton.text("Add to Team")
+        pokeImg.attr("src", image);
+        var pokeName = $("<h1>").text("Name: " + cap + " #" + number);
+        //getting base stats to add into the statDiv element
         for (i = 0; i < baseStat.length; i++) {
-          console.log(response.stats[i].stat.name);
-          console.log(baseStat[i].base_stat);
           var sName = response.stats[i].stat.name
           sName = sName.toUpperCase()
           var sNum = baseStat[i].base_stat
@@ -41,6 +34,7 @@ var search = "420";
           statDiv.append(p);
           statDiv.append(p2);
         }
+        //getting pokemon types or type to add to typeDiv
         for (i = 0; i < response.types.length; i++) {
           var pEl = response.types[i].type.name
           var pElCap = pEl.charAt(0).toUpperCase() + pEl.slice(1)
@@ -48,8 +42,7 @@ var search = "420";
           typeDiv.append(p)
 
         }
-        pokeImg.attr("src", image);
-        var pokeName = $("<h1>").text("Name: " + cap + " #" + number);
+        //apending all the info into 1 div in order then adding that div to the html
         pokeDiv.append(pokeName);
         pokeDiv.append(pokeImg);
         pokeDiv.append(typeDiv);
